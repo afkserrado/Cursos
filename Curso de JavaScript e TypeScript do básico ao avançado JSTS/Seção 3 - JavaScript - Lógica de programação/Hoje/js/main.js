cb29b29a50f9794b4a4506e9f47b573b632bb4e1
Anderson Serrado <akserrado@gmail.com>
2025-09-09 23:11:04 -0300

    Att

---
+++ Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 3 - JavaScript - Lógica de programação/Hoje/js/main.js
@@ -0,0 +1,35 @@
+// Data de hoje
+const hoje = new Date(Date.now())
+console.log(hoje)
+
+// Formata a data
+const dataFormatada = formatarData(hoje)
+console.log(dataFormatada)
+
+function formatarData(data) {
+    const dia = data.getDate()
+    const mes = data.getMonth()
+    const ano = data.getFullYear()
+    const hora = data.getHours()
+    const min = data.getMinutes()
+    const diaSemana = data.getDay()
+
+    // Dias da semana por extenso
+    const diasSemana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"
+    ];
+
+    // Meses do ano por extenso
+    const meses = ["janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"];
+
+    const diaSemanaExtenso = diasSemana[diaSemana]
+    const mesExtenso = meses[mes]
+
+    // Horas e minutos com zero à esquerda
+    const horaCorrigida = hora >= 10 ? hora : `0${hora}`
+    const minCorrigido = min >= 10 ? min : `0${min}`
+
+    return `${diaSemanaExtenso}, ${dia} de ${mesExtenso} de ${ano} ${horaCorrigida}:${minCorrigido}` 
+}
+
+const data = document.querySelector("#data")
+data.innerHTML = dataFormatada
