0b5021006a1fd2670ca045c9fcb570fe5b1494a3
Anderson Serrado <akserrado@gmail.com>
2025-09-25 01:41:12 -0300

    Att

--- Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 5 - Arrays (Avançado)/index.js
+++ Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 5 - Arrays (Avançado)/index.js
@@ -57,10 +57,78 @@ console.log(nomes, removidos)
 
 /*
 Aula 83 - Concatenando arrays
-*/
 
 const a1 = [1, 2, 3]
 const a2 = [4, 5, 6]
 //const a3 = a1.concat(a2, 25)
 const a3 = [...a1, ...a2, 27, ...[16, 14, 100]]
 console.log(a3)
+*/
+
+/*
+Aula 84 - Filter, map, reduce
+*/
+
+// Filter
+// Filter automaticamente percorre os elementos do array
+// Filter sempre retorna um array
+// Arrow function: valor => valor > 10. Como só tem 1 linha, o retorno é implícito
+/*
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+const numFiltered = numeros.filter(valor => valor > 10)
+console.log(numFiltered)
+
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Letícia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47},
+]
+
+const pessoasFiltered = pessoas.filter(obj => obj.nome.length >= 5)
+console.log(pessoasFiltered)
+
+const ageFiltered = pessoas.filter(obj => obj.idade > 50)
+console.log(ageFiltered)
+
+const nameFiltered = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('a'))
+console.log(nameFiltered)
+*/
+
+// Map
+// Map sempre retorna um array
+// Filter retorna um vetor menor ou igual ao original, sem modificar o valor dos elementos
+// Map retorna um vetor do mesmo tamanho do original, mas com seus valores modificados por alguma operação
+ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
+
+ const numDobro = numeros.map(valor => valor * 2)
+ console.log(numDobro)
+
+ const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Letícia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace', idade: 47},
+]
+
+const nomes = pessoas.map(obj => obj.nome)
+console.log(nomes)
+
+/* const idades = pessoas.map(function(obj) {
+    delete obj.nome
+    return obj
+})
+console.log(idades)
+
+const idades2 = pessoas.map(obj => ({idade: obj.idade}))
+console.log(idades2) */
+
+const ids = pessoas.map((obj, index) => ({...obj, id: index}))
+console.log(ids)
+
+console.log(pessoas)
+
