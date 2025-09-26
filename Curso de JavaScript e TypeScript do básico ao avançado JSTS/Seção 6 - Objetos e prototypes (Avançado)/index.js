8f531407058d044a2d5a24016a50e27d4e8881ad
Anderson Serrado <akserrado@gmail.com>
2025-09-26 20:30:38 -0300

    Att

--- Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 6 - Objetos e prototypes (Avançado)/index.js
+++ Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 6 - Objetos e prototypes (Avançado)/index.js
@@ -67,3 +67,86 @@ function Pessoa(nome, sobrenome) {
 const p1 = new Pessoa('Fulano', 'de Tal')
 console.log(p1.nomeCompleto())
 */
+
+/*
+defineProperty e defineProperties
+*/
+/*
+function Produto(nome, preco, estoque) {
+    this.nome = nome
+    this.preco = preco
+    this.estoque = estoque
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // Mostra a chave
+        value: function() {
+            return estoque
+        },
+        writable: false, // Alterar
+        configurable: false // Configurar (deletar, por ex)
+    })
+
+    Object.defineProperties(this, {
+        nome: {
+            enumerable: true,
+            value: nome,
+            writable: true,
+            configurable: true
+        },
+
+        preco: {
+            enumerable: true,
+            value: preco,
+            writable: true,
+            configurable: true
+        },
+    })
+}
+
+const p1 = new Produto('Camiseta', 20, 3)
+p1.estoque = 500000
+//delete p1.estoque
+console.log(p1)
+*/
+
+/*
+Getters e setters
+*/
+/*
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    let estoquePrivado = estoque;
+    
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,  // Mostra a chave
+        configurable: true,  // Configurar (deletar, por ex)
+        get: function() {
+            return estoquePrivado;
+        },
+
+        set: function(valor) {
+            if (typeof valor !== 'number') {
+                console.log('Bad value');
+                return;
+            }
+            estoquePrivado = valor;  // Atualiza estoquePrivado com o valor passado
+        }
+    });
+}
+
+const p1 = new Produto('Camiseta', 20, 3);
+p1.estoque = '500000';  // Tenta atribuir uma string (não será permitido)
+console.log(p1.estoque);  // 3, pois o valor não foi alterado
+
+p1.estoque = 500000;  // Agora atribui um número
+console.log(p1.estoque);  // 500000, pois o valor foi alterado com sucesso
+*/
+
+/*
+Métodos para objetos
+*/
+
+
+
