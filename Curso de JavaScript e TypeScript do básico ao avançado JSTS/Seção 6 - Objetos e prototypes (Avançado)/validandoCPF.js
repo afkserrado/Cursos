5db42f386a460d971be77142e098530bea06addf
Anderson Serrado <akserrado@gmail.com>
2025-09-28 02:11:32 -0300

    Att

--- Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 6 - Objetos e prototypes (Avançado)/validandoCPF.js
+++ Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 6 - Objetos e prototypes (Avançado)/validandoCPF.js
@@ -14,6 +14,7 @@ cpfValido.prototype.valida = function() {
     //console.log(this.cpfLimpo)
     if (this.cpfLimpo === 'undefined') return false
     if (this.cpfLimpo.length !== 11) return false
+    if (this.isSequencia() === true) return false
 
     // Extrai os primeiros 9 dígitos
     let cpfParcial = this.cpfLimpo.slice(0, -2)
@@ -23,7 +24,7 @@ cpfValido.prototype.valida = function() {
     //console.log(dv1, Number(this.cpfLimpo[9]))
 
     // Verifica o primeiro dígito verificador
-    if (dv1 !== Number(this.cpfLimpo[9])) return `1 - ${false}`
+    if (dv1 !== Number(this.cpfLimpo[9])) return false
 
     // Extrai os primeiros 10 dígitos
     cpfParcial = this.cpfLimpo.slice(0, -1)
@@ -33,7 +34,7 @@ cpfValido.prototype.valida = function() {
     //console.log(dv2, Number(this.cpfLimpo[10]))
 
     // Verifica o segundo dígito verificador
-    if (dv2 !== Number(this.cpfLimpo[10])) return `2 - ${false}`
+    if (dv2 !== Number(this.cpfLimpo[10])) return false
 
     return true
 }
@@ -54,7 +55,20 @@ cpfValido.prototype.criaDigito = function(cpfParcial) {
     return (soma % 11 < 2) ? 0 : 11 - (soma % 11)
 }
 
-//const cpf = new cpfValido('705.484.450-52')
-const cpf = new cpfValido('026.379.495-46')
+// Método
+cpfValido.prototype.isSequencia = function() {
+    for (let char of this.cpfLimpo) {
+        //console.log(char)
+        if (char !== this.cpfLimpo[0]) {
+            return false
+        }
+    }
+    return true
+}
+
+const cpf = new cpfValido('705.784.451-52')
+const cpf2 = new cpfValido('111.111.111-11')
+const cpf3 = new cpfValido('070.987.720-03')
 console.log(cpf.valida())
-console.log(cpf)
+console.log(cpf2.valida())
+console.log(cpf3.valida())
