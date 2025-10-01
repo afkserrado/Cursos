ccaa5aa26e2374f6e6e4bb5dfb7510fb28406518
Anderson Serrado <akserrado@gmail.com>
2025-10-01 12:44:05 -0300

    Att

---
+++ Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 7 - Classes/index.js
@@ -0,0 +1,27 @@
+/*
+Nas classes, os métodos criados automaticamente fazem parte do prototype da classe. Assim, quaisquer instâncias daquelas classes usarão os métodos da classe pai, ao invés de replicá-los.
+*/
+
+// Classes
+/*
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+
+    falar() {
+        console.log(`${this.nome} está falando.`)
+    }
+
+    comer() {
+        console.log(`${this.nome} está comendo.`)
+    }
+}
+
+const p1 = new Pessoa('Fulano', 'Beltrano')
+console.log(p1)
+p1.falar()
+*/
+
+// Getters e setters
