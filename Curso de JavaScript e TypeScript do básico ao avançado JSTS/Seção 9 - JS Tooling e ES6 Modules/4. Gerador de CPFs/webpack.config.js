36c3d2dbc6bf641d0208888712c91bd8ba488552
Anderson Serrado <akserrado@gmail.com>
2026-03-03 19:16:51 -0300

    first commit

---
+++ Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 9 - JS Tooling e ES6 Modules/4. Gerador de CPFs/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js',
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
