66591829921c5b58dd17d9f8fb60db4b4a0328a4
Anderson Serrado <akserrado@gmail.com>
2026-03-09 22:20:44 -0300

    Att

---
+++ Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 10 - Node, Npm, Express e MongoDB/7. Models/server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express(); // Cria a instância do servidor
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        console.log('Conexão estabelecida com a base de dados.');
+        app.emit('pronto');
+    })
+    .catch(e => console.log(e));
+
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware')
+
+// Middleware para ler dados de formulário
+app.use(express.urlencoded({extended: true}));
+
+// Middleware que tenta servir arquivos estáticos da pasta "public"
+// Se encontrar o arquivo, envia ao cliente; se não, passa para as rotas
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+// Define o local dos templates HTML
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+
+// Define o motor dos templates HTML
+app.set('view engine', 'ejs');
+
+app.use(middlewareGlobal);
+app.use(routes);
+
+app.on('pronto', () => {
+    // Inicia o servidor e escuta as requisições numa porta específica
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000');
+        console.log('Servidor executando na porta 3000');
+    });
+})
