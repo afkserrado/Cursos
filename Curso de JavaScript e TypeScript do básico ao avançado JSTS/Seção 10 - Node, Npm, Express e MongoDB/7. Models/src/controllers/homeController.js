ae54b34b7d1a30b70a6e11cafd8b218095bb0edf
Anderson Serrado <akserrado@gmail.com>
2026-03-09 22:34:10 -0300

    Att

--- Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 10 - Node, Npm, Express e MongoDB/7. Models/src/controllers/homeController.js
+++ Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 10 - Node, Npm, Express e MongoDB/7. Models/src/controllers/homeController.js
@@ -1,3 +1,12 @@
+const HomeModel = require('../models/homeModel');
+
+HomeModel.create({
+    titulo: 'Teste',
+    descricao: 'Descricao'
+})
+    .then(dados => console.log(dados))
+    .catch(e => console.log(e));
+
 exports.paginaInicial = (req, res) => {
     res.render('index');
     return;
---
+++ Curso de JavaScript e TypeScript do básico ao avançado JSTS/Seção 10 - Node, Npm, Express e MongoDB/7. Models/src/models/HomeModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Estrutura do documento que será salvo no banco
+const HomeSchema = new mongoose.Schema({
+    titulo: { type: String, required: true },
+    descricao: String
+});
+
+// Criação do 'Model', objeto que permitirá a interação do programa com a coleção no banco
+const HomeModel = mongoose.model('Home', HomeSchema);
+
+module.exports = HomeModel;
